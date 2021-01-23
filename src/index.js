b03b341c709f051e7b0fe626451b7aedc350d22a
Ben Cooper <benjamin.cooper@convoy.com>
2021-01-23 17:23:02 -0600

    add metadata

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ require('dotenv').config();
 const { watch } = require('./watcher');
 const { collect } = require('./collectMetadata');
 const { uploadImage } = require('./transports/s3');
+const { add } = require('date-fns');
 
 const DIRECTORY_TO_WATCH = './';
 
-const processImage = async path => {
+const processImage = async (path, additionalMetadata) => {
   const { answers } = await collect(path);
   const { title, ...rest } = answers;
 
   try {
-    await uploadImage(path, `${title}.jpeg`, rest);
+    await uploadImage(path, `${title}.jpeg`, { ...rest, ...additionalMetadata });
     console.log(`Uploaded ${title}.jpeg`);
   } catch (err) {
     console.error('Failed upload', path, err);
@@ -20,7 +21,8 @@ const processImage = async path => {
 };
 
 const main = async () => {
-  watch(DIRECTORY_TO_WATCH, processImage);
+  const grade = process.argv[2] ?? 'unspecified';
+  watch(DIRECTORY_TO_WATCH, path => void processImage(path, { grade }));
 };
 
 main().catch(err => {
